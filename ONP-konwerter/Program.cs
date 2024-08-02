ac04f6f39cf1a93f54e79d74d2bdbf3783a956c4
Fidoxas <143958956+Fidoxas@users.noreply.github.com>
2024-08-03 01:03:00 +0200

    Dodane Obliczanie wyniku po przekształceniu dzialania na ONP
    dodałem script ResultCounter ktory liczy wynik działania

---
+++ ONP-konwerter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ONP_konwerter;
+
+var inputReader = new InputReader();
+var inputChecker = new InputChecker();
+var converter = new Converter();
+var er = new EqualitionReader();
+
+var op = inputReader.TakeOp();
+Console.WriteLine("your operation " + op);
+
+if (inputChecker.AccetableOperation(op))
+{
+    var output = converter.PrerpareOperation(op);
+    er.ReadEquation(output);
+}
+else
+{
+    inputChecker.PrintAllowedOperators();
+}
---
+++ ONP-konwerter/ResultCounter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ONP_konwerter;
+
+public class ResultCounter
+{
+    public static string ResultOnpDefiner(List<String> OnpEq)
+    {
+        List<String> Output = new List<string>(OnpEq);;
+        while (Output.Count > 1)
+        {
+            for (int i = 0; i < Output.Count; i++)
+            {
+                if (EqualitionDefiner.MarksPriorities.ContainsKey(Output[i].First()))
+                {
+                    string result = CalculateResult(Output.GetRange(i - 2, 3));
+                    Output.RemoveRange(i - 2, 3);
+                    Output.Insert(i - 2, result);
+                    break;
+                }
+            }
+        }
+        return Output.First();
+    }
+
+    static string CalculateResult(List<String> operation)
+    {
+        int operand1 = int.Parse(operation[0]);
+        int operand2 = int.Parse(operation[1]);
+        char operatorChar = operation[2].First();
+        
+        int result = operatorChar switch
+        {
+            '+' => operand1 + operand2,
+            '-' => operand1 - operand2,
+            '*' => operand1 * operand2,
+            '/' => operand1 / operand2,
+            _ => throw new InvalidOperationException("Niepoprawny operator")
+        };
+
+        return result.ToString();
+    }
+}
