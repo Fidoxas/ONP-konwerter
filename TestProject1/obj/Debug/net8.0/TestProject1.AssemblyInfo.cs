bbf02ef6a56365e390005926318b465873ee2035
Fidoxas <143958956+Fidoxas@users.noreply.github.com>
2024-10-14 16:19:28 +0200

    nic

--- TestProject1/obj/Debug/net8.0/TestProject1.AssemblyInfo.cs
+++ TestProject1/obj/Debug/net8.0/TestProject1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2e49f826f27c7762b8afcb77baaea1423758f1c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c51380244d5fee887f6ab3b4aac2ef93d4adf5f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
